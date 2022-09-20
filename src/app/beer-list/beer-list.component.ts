7b08fc6835b4344d0ef2463aca0bda56f3ab0ef4
Mauro Rivera <mauroriverae@gmail.com>
2022-09-19 23:04:26 -0300

    agrega interfaz beer y template del componente beer-list

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -1,18 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Beer } from './Beer'
 @Component({
   selector: 'app-beer-list',
   templateUrl: './beer-list.component.html',
   styleUrls: ['./beer-list.component.scss']
 })
 export class BeerListComponent implements OnInit {
-  beer = {
-    "name": "negra fuerte",
-    "type": "porter",
-    "price": 123,
-    "stock": 5,
-    "image": "assets/porter.jpg"
-  }
+  beer: Beer [] = [
+    {
+      name: "Biter Call Saul",
+      type: "IPA",
+      price: 180,
+      stock: 5,
+      image: "assets/porter.jpg",
+      clearance: false,
+    },
+    {
+      name: "Red Red Wine",
+      type: "Barley Wine",
+      price: 200,
+      stock: 3,
+      image: "assets/porter.jpg",
+      clearance: true,
+    },
+    {
+      name: "Yellow Submarine",
+      type: "Golden Ale",
+      price: 180,
+      stock: 0,
+      image: "assets/porter.jpg",
+      clearance: false,
+    },
+    {
+      name: "Session IPA",
+      type: "IPA",
+      price: 250,
+      stock: 2,
+      image: "assets/porter.jpg",
+      clearance: false,
+    },
+  ] 
   constructor() { }
 
   ngOnInit(): void {
